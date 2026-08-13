512f76f8fceabbb5d57f54920fea412939d9bcf6
amirparadox83 <amirpepe9001@gmail.com>
2026-08-13 20:52:12 +0330

    Fix stateProvider signature in mistake_providers

--- lib/presentation/providers/mistake_providers.dart
+++ lib/presentation/providers/mistake_providers.dart
@@ -30,7 +30,7 @@ final mistakeListProvider = FutureProvider<List<Mistake>>(
 
 /// Optional reason filter.
 final mistakeReasonFilterProvider =
-    stateProvider<MistakeReason?>((ref) => null);
+    stateProvider<MistakeReason?>(() => null);
 
 /// Filtered list (applies reason filter on top of `mistakeListProvider`).
 final filteredMistakeListProvider = Provider<List<Mistake>>((ref) {
