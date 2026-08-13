ee496367688843a2c9d1a38fd666612bb7cc3218
amirparadox83 <amirpepe9001@gmail.com>
2026-08-13 20:26:31 +0330

    Add riverpod_state_notifier import

--- lib/presentation/providers/core_providers.dart
+++ lib/presentation/providers/core_providers.dart
@@ -4,6 +4,7 @@ library presentation.providers.core_providers;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_state_notifier/riverpod_state_notifier.dart';
 
 import '../../data/database/app_database.dart';
 import '../../data/file_storage/file_storage_service.dart';
