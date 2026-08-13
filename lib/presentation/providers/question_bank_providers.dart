16358f697105a1dbff3492cfd43438d7a864775d
amirparadox83 <amirpepe9001@gmail.com>
2026-08-13 20:26:23 +0330

    Add riverpod_state_notifier import

--- lib/presentation/providers/question_bank_providers.dart
+++ lib/presentation/providers/question_bank_providers.dart
@@ -3,6 +3,7 @@
 library presentation.providers.question_bank_providers;
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_state_notifier/riverpod_state_notifier.dart';
 
 import '../../domain/entities/entities.dart';
 import 'repository_providers.dart';
